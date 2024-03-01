a840e9b3c421695b3273853c6f97a81a43984059
Noe1017 <jixiangli1017@gmail.com>
2024-03-01 21:31:34 +0800

    new server

--- src/main/java/com/atguigu/case1/DistributeServer.java
+++ src/main/java/com/atguigu/case1/DistributeServer.java
@@ -1,13 +1,46 @@
 package com.atguigu.case1;
 
+import java.io.IOException;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.ZooDefs.Ids;
 public class DistributeServer {
+    private static String connectString =
+            "hadoop102:2181,hadoop103:2181,hadoop104:2181";
+    private static int sessionTimeout = 2000;
+    private ZooKeeper zk = null;
+    private String parentNode = "/servers";
+    // 创建到 zk 的客户端连接
+    public void getConnect() throws IOException{
+        zk = new ZooKeeper(connectString, sessionTimeout, new
+                Watcher() {
+                    @Override
+                    public void process(WatchedEvent event) {
 
-    public static void main(String [] args){
-
-        // 1 获取zk连接
-
-        // 2 注册服务器zk集群
-
-        // 3 启动业务逻辑（睡觉)
+                    }
+                });
+    }
+    // 注册服务器
+    public void registServer(String hostname) throws Exception{
+        String create = zk.create(parentNode + "/server",
+                hostname.getBytes(), Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println(hostname +" is online "+ create);
+    }
+    // 业务功能
+    public void business(String hostname) throws Exception{
+        System.out.println(hostname + " is working ...");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+    public static void main(String[] args) throws Exception {
+// 1 获取 zk 连接
+        DistributeServer server = new DistributeServer();
+        server.getConnect();
+// 2 利用 zk 连接注册服务器信息
+        server.registServer(args[0]);
+// 3 启动业务功能
+        server.business(args[0]);
     }
 }
